a97a235bad48337a474df4178fc7bcf73ad4248a
Lance Fallon <fallon.lance@gmail.com>
2019-07-03 16:05:50 -0400

    Update fireinit.js

--- services/fireinit.js
+++ services/fireinit.js
@@ -13,9 +13,6 @@ var config = {
   appId: "1:249461641393:web:b0d7b3920d50ccae"
 }
 
-//webclientid => 925146945924-od5n5li9rs5spkcrd4achdbb0l6dir93.apps.googleusercontent.com
-//webclientsecret => ODkuvu3OvJzwy2Aa6EOQz_6H
-
 firebase.initializeApp(config)
 export const GoogleProvider = new firebase.auth.GoogleAuthProvider()
 export const auth = firebase.auth()
